fa9feaf7a2aafc2e8e2e3386b1568bfd0c36e884
michel <michel.pawlak@gmail.com>
2015-09-26 00:11:55 +0200

    migrate parent pom and fix license

--- conversion/src/main/java/com/qualinsight/mojo/cobertura/transformation/CoberturaToSonarQubeCoverageReportConverter.java
+++ conversion/src/main/java/com/qualinsight/mojo/cobertura/transformation/CoberturaToSonarQubeCoverageReportConverter.java
@@ -1,18 +1,21 @@
 /*
- * This file is part of qualinsight-mojo-cobertura-conversion.
+ * qualinsight-mojo-cobertura
+ * Copyright (c) 2015, QualInsight
+ * http://www.qualinsight.com/
  *
- * qualinsight-mojo-cobertura-conversion is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
  *
- * qualinsight-mojo-cobertura-conversion is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with qualinsight-mojo-cobertura-conversion.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, you can retrieve a copy
+ * from <http://www.gnu.org/licenses/>.
  */
 package com.qualinsight.mojo.cobertura.transformation;
 
