c76a3918e326af62b10b9798bb4e3796dc58ed27
michel <michel.pawlak@gmail.com>
2015-09-25 08:23:40 +0200

    added missing license header

--- conversion/src/main/java/com/qualinsight/mojo/cobertura/transformation/CoberturaToSonarQubeCoverageReportConversionProcessingException.java
+++ conversion/src/main/java/com/qualinsight/mojo/cobertura/transformation/CoberturaToSonarQubeCoverageReportConversionProcessingException.java
@@ -1,3 +1,19 @@
+/*
+ * This file is part of qualinsight-mojo-cobertura-conversion.
+ *
+ * qualinsight-mojo-cobertura-conversion is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * qualinsight-mojo-cobertura-conversion is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with qualinsight-mojo-cobertura-conversion.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.qualinsight.mojo.cobertura.transformation;
 
 public class CoberturaToSonarQubeCoverageReportConversionProcessingException extends Exception {
